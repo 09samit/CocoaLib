24f92eaf701f30fc5b0bf01c67fe842820a6da00
Amit Garg <amitg@ohumhealthcare.com>
2020-07-02 23:23:50 +0530

    Setuo framework
    Add framework for service.

---
+++ CocoaLib/CocoaLib/CocoaLib.h
@@ -0,0 +1,19 @@
+//
+//  CocoaLib.h
+//  CocoaLib
+//
+//  Created by Rakesh Patole on 02/07/20.
+//  Copyright © 2020 Amit Garg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CocoaLib.
+FOUNDATION_EXPORT double CocoaLibVersionNumber;
+
+//! Project version string for CocoaLib.
+FOUNDATION_EXPORT const unsigned char CocoaLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CocoaLib/PublicHeader.h>
+
+
---
+++ CocoaLib/CocoaLib/Extension.swift
@@ -0,0 +1,21 @@
+//
+//  Extension.swift
+//  CocoaLib
+//
+//  Created by Rakesh Patole on 02/07/20.
+//  Copyright © 2020 Amit Garg. All rights reserved.
+//
+
+import Foundation
+
+public class Service {
+    
+    public init() {
+        
+    }
+    
+    func hello() {
+        print("Hello my frind from cocoa framework")
+    }
+    
+}
